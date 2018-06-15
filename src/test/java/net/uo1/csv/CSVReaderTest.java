c87f872a99f35b1bd472d9f703f77f2959ffdc43
Azazar <m@uo1.net>
2018-06-15 11:07:03 +0300

    strip cr in eof option

--- src/test/java/net/uo1/csv/CSVReaderTest.java
+++ src/test/java/net/uo1/csv/CSVReaderTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
  */
 public class CSVReaderTest {
     public static void main(String[] args) throws Exception {
-        String test = "14\r,,http://thumbs.pornoplum.com/contents/videos_screenshots/0/14/352x198/1.jpg,MILF nice-looking to solicit,\"MILF, Big Boobs, Brunettes, MILFs, Blowjobs, Mom, Russian\",\"Big Tits,MILF,Mature,Russian,Mom,big boobs,straight\",\"<iframe width=\\\"640\\\" height=\\\"480\\\" src=\\\"http://www.pornoplum.com/embed/14\\\" frameborder=\\\"0\\\" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>\",,,milf-nice-looking-to-solicit\r\n";
+        String test = "14\r,\r,http://thumbs.pornoplum.com/contents/videos_screenshots/0/14/352x198/1.jpg,MILF nice-looking to solicit,\"MILF, Big Boobs, Brunettes, MILFs, Blowjobs, Mom, Russian\",\"Big Tits,MILF,Mature,Russian,Mom,big boobs,straight\",\"<iframe width=\\\"640\\\" height=\\\"480\\\" src=\\\"http://www.pornoplum.com/embed/14\\\" frameborder=\\\"0\\\" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>\",,,milf-nice-looking-to-solicit\r\n";
         
         try (CSVReader r = new CSVReader(new StringReader(test))) {
             r.setStripCrBeforeLf(true);
